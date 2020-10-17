600614c94c7e3295172d817548946527601e3631
Douglas Anderson <hockeybuggy@gmail.com>
2020-10-17 18:51:51 -0400

    Serialize story states

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod epic_info {
     use serde::{Deserialize, Serialize};
+    // use std::collections::HashMap;
 
     const BASE_URL: &str = "https://www.pivotaltracker.com/services/v5";
 
@@ -11,10 +12,32 @@ mod epic_info {
     }
 
     #[derive(Serialize, Deserialize, Debug)]
+    pub enum StoryState {
+        #[serde(alias = "accepted")]
+        Accepted,
+        #[serde(alias = "delieved")]
+        Delivered,
+        #[serde(alias = "finished")]
+        Finished,
+        #[serde(alias = "started")]
+        Started,
+        #[serde(alias = "rejected")]
+        Rejected,
+        #[serde(alias = "planned")]
+        Planned,
+        #[serde(alias = "unstarted")]
+        Unstarted,
+        #[serde(alias = "unscheduled")]
+        Unscheduled,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
     pub struct Story {
         pub id: u64,
         pub project_id: u64,
         pub name: String,
+        pub url: String,
+        pub current_state: StoryState,
         pub blocked_story_ids: Vec<u64>,
     }
 
@@ -28,7 +51,7 @@ mod epic_info {
             .header("X-TrackerToken", tracker_token)
             .send()
             .await?;
-        Ok(response)
+        return Ok(response);
     }
 
     pub async fn get_epics_with_label(
@@ -38,8 +61,27 @@ mod epic_info {
         let epics: Vec<Epic> = response.json().await?;
         println!("{:?}", epics);
 
-        Ok(epics)
+        return Ok(epics);
     }
+
+    // fn reverse_blocker_links(stories: Vec<Story>) -> Vec<Story> {
+    //     let mut story_id_map = HashMap::new();
+
+    //     for s in stories.iter() {
+    //         story_id_map.insert(s.id, s);
+    //     }
+
+    //     for s in stories.iter() {
+    //         for blocked_id in s.blocked_story_ids {
+    //             match story_id_map.get(&blocked_id) {
+    //                 Some(blocking_story) => {}
+    //                 None => {}
+    //             }
+    //         }
+    //     }
+
+    //     return story_id_map.into_iter().map(|(_, s)| *s).collect();
+    // }
 
     pub async fn get_stories_with_label(
         epic_label: &str,
@@ -50,23 +92,65 @@ mod epic_info {
         ))
         .await?;
         let stories: Vec<Story> = response.json().await?;
-        println!("{:?}", stories);
 
-        Ok(stories)
+        // let augmented_stories = reverse_blocker_links(stories);
+        // return Ok(augmented_stories);
+        return Ok(stories);
     }
 }
 
 mod diagram_text_emitter {
     use super::epic_info;
 
-    pub fn dot_representation(epic: &epic_info::Epic, stories: &Vec<epic_info::Story>) -> String {
-        let story_ids: Vec<String> = stories
-            .into_iter()
-            .map(|story| format!("{}", story.id))
-            .collect();
-        format!("{}\nstories: {:?}", epic.name, story_ids)
+    fn prelude() -> String {
+        return "\
+            graph TD\n\
+            \tclassDef STARTED fill:#deebff,stroke:0747a6;\n\
+            \tclassDef DONE fill:#e3fcef,stroke:#064;\n\
+            "
+        .to_owned();
+    }
+
+    fn story_node(story: &epic_info::Story) -> String {
+        let node_id = format!("{}", &story.id);
+        let status = match &story.current_state {
+            &epic_info::StoryState::Accepted => ":::DONE",
+            &epic_info::StoryState::Delivered => ":::DONE",
+            &epic_info::StoryState::Finished => ":::DONE",
+            _ => "",
+        };
+        let link = format!("click {} '{}' '{}'", &story.id, &story.url, &story.name);
+        let deps = &story
+            .blocked_story_ids
+            .iter()
+            .map(|blocked_id| {
+                return format!("\t{from} --> {to}\n", from = &story.id, to = blocked_id);
+            })
+            .collect::<Vec<String>>()
+            .join("");
+
+        return format!(
+            "\
+            \t{node_id}{status}\n\
+            \t{link}\n\
+            {deps}\
+            \n",
+            node_id = node_id,
+            status = status,
+            link = link,
+            deps = deps,
+        );
+    }
+
+    pub fn dot_representation(_epic: &epic_info::Epic, stories: &Vec<epic_info::Story>) -> String {
+        let story_nodes: String = stories.into_iter().map(story_node).collect();
+        return format!("{}\n{}", prelude(), story_nodes);
     }
 }
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 use dotenv;
 
@@ -88,8 +172,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let dot_diagram: String = diagram_text_emitter::dot_representation(&epics[0], &stories);
 
-    println!("Diagram: {}", dot_diagram);
+    let path = Path::new("epic_diagram.mmd");
+    let display = path.display();
+    println!("Writing Diagram to file...");
 
-    // TODO generate represntation in a textual format (dot? mermaidjs? plantuml?)
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    match file.write_all(dot_diagram.as_bytes()) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+
     Ok(())
 }
